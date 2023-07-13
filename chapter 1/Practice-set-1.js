a88b30d2f6232075ae151c1c00ecb750eec0dc6e
Anshuman Praharaj <anshumanprof01@gmail.com>
2023-07-13 14:55:21 +0530

    updated chapter 1

--- chapter 1/Practice-set-1.js
+++ chapter 1/Practice-set-1.js
@@ -2,7 +2,18 @@
 // answer:
 let string_var="hello this is question";
 let number_var=1;
-console.log(string_var+' '+number_var);
+// console.log(string_var+' '+number_var); //run this to get the answer
 // question two : find the data type of the variable 'string_var' created in question one
 // answer:
-console.log(typeof(string_var));
+console.log(typeof(string_var)); //run this to get the answer
+// question three : create a const object try to change it later!
+const object_var={
+    name:"john",
+    age:20
+    
+}
+// object_var=17; // this will throw an error as we are trying to change the value of a const variable [TypeError: Assignment to constant variable.]
+// question four : create a const array try to change it later!
+// object_var.name="jane"; //this will not throw an error as we are not changing the value of the const variable but the value of the object inside the const variable
+console.log(object_var);
+
