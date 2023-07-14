f7e93ca414a04dcc12b9c994a88946de64b5d3bd
anshumancodes <anshumanprof01@gmail.com>
2023-07-14 12:47:24 +0530

    added chapter 2

---
+++ chapter 2/OperatorsandExpressions.js
@@ -0,0 +1,93 @@
+// operators and expressions in javascript
+
+
+
+
+
+//  Arithmetic Operators
+
+let a=40;
+let b=5;
+
+
+// console.log("a+b= " + a+b);
+// console.log("a-b= " + (a-b));
+// console.log("a*b= " + a*b);
+// console.log("a/b= " + a/b);
+// console.log("a%b= " + a%b);
+// console.log("a**b ="+ a**b);     //run to see the output
+
+// let c=10;
+//                            //values
+// console.log("++c = ",++c); //11
+// console.log("c++ = ",c++); //11 //(12)
+// console.log("--c = ",--c); //11 (12-11)
+// console.log("c-- = ",c--); //11 
+// console.log("c = ",c); 10
+// console.log("c-- = ",c--); //11       //run to see the output
+
+
+
+
+// assignment operators
+
+let d=10;
+
+// console.log("d+=5 = ",d+=5);
+// console.log("d-=5 = ",d-=5);
+// console.log("d*=5 = ",d*=5);
+// console.log("d/=5 = ",d/=5);
+// console.log("d%=5 = ",d%=5);
+// console.log("d**=5 = ",d**=5); //run to see the output
+
+
+// comparison operators
+
+let e=17;
+let f=10;
+let g="10"
+
+
+// example 1
+console.log(e==f)
+// output: e is not equal to f 
+
+// example 2
+console.log(f==g)
+
+// output: true, but how? because == only checks the value not the type of the variable
+
+// example 3
+console.log(f===g)
+// output: false, because === checks the value and the type of the variable
+// while the value of f and g is same but the type is different so it will give false
+
+// example 4
+console.log(f!=g)
+// output: false, because != only checks the value not the type of the variable
+
+// example 5
+console.log(f!==g)
+// output: true, because !== checks the value and the type of the variable
+
+// example 6
+console.log(e>f)
+// output: true, because e is greater than f
+
+// example 7
+console.log(e<f)
+// output: false, because e is greater than f
+
+// example 8
+console.log(e>=f)
+// output: true, because e is greater than f
+
+// example 9
+console.log(e<=f)
+// output: false, because e is greater than f
+
+
+
+
+
+
