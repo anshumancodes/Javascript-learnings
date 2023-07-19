550171acc234c96f4fa0e7c7977d8a663813fee2
anshumancodes <anshumanprof01@gmail.com>
2023-07-20 00:15:39 +0530

    added chapter 4- updated strings.js

--- chapter 4/strings.js
+++ chapter 4/strings.js
@@ -12,3 +12,37 @@ console.log(typeof(myString)); // output : string
 // here is how to check the length of a string
 
 console.log(myString.length); // output : 16 [including spaces as they are also characters]
+
+
+
+// here is how to access a character in a string
+
+console.log(myString[0]); // output : T
+console.log(myString[1]); // output : h
+console.log(myString[2]); // output :  i
+
+// indexing in strings starts from 0 and ends at length-1 , it also includes spaces as they are also characters
+
+
+
+// Template literals
+
+// Template literals are used to embed expressions into strings
+// Template literals are enclosed by the backtick (` `) (grave accent) character instead of double or single quotes.
+
+// lets see a example
+
+let userName = "Anshuman";
+let age = 20;
+
+let data = `my name is ${userName} and my age is ${age}`; // this is a template literal 
+
+//using variables inside a string directly is called  string interpolation
+
+console.log(data)
+
+
+// inside a we can use both single quotes and double quotes without any problem
+
+
+
