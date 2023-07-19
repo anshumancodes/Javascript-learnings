50276d25e8516c58ea8d389542327c1e57b592ca
anshumancodes <anshumanprof01@gmail.com>
2023-07-19 17:59:09 +0530

    added chapter 4- added strings.js

---
+++ chapter 4/strings.js
@@ -0,0 +1,14 @@
+// strings in javascript
+
+
+// strings is a primitive data type 
+
+
+let myString = "This is a string";
+console.log(typeof(myString)); // output : string
+
+// we can use single quotes or double quotes to define a string
+
+// here is how to check the length of a string
+
+console.log(myString.length); // output : 16 [including spaces as they are also characters]
