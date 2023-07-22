e075ba4b31a4a1b8974617457cc11e1764fbd1c2
anshumancodes <anshumanprof01@gmail.com>
2023-07-23 00:00:31 +0530

    updated  practice set 4

---
+++ chapter 4/practice-set-4.js
@@ -0,0 +1,23 @@
+// find the length of  string "har\""  
+// answer : 4 as \ is escape character
+console.log("har\"".length)
+
+
+// convert a string to lowercase
+
+let str = "Harsh";
+
+console.log(str.toLowerCase())
+
+
+// extract the space our the string
+
+let str2= " bana na ";
+
+console.log(str2.trim())
+
+// try to change a charter in string
+
+let str3 = "Harsh";
+
+console.log(str3.replace("H","p"))
