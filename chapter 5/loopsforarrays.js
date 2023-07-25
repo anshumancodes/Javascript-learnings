75577b37db110b5c63fa19073d34dcc315776928
anshumancodes <anshumanprof01@gmail.com>
2023-07-25 11:07:17 +0530

    updated loopforarrays.js

---
+++ chapter 5/loopsforarrays.js
@@ -0,0 +1,15 @@
+// iterating over arrays using while loop
+let eachoverScores=[12,1,13,41,67,90]
+let i=0;
+while( i<eachoverScores.length){
+ 
+    console.log("score for the over is "+eachoverScores[i])
+    i++
+}
+
+// iterating over arrays using do while loop
+
+do{
+    console.log("score for the over is "+eachoverScores[i]);
+    i++
+}while(i<=eachoverScores.length)
